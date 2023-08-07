bfa832a86e1a12cdc452081ded48516fd179ca7b
abhijitCogoport <abhijit.gayen@cogoport.com>
2023-08-07 16:18:24 +0530

    no of epoch

--- train.py
+++ train.py
@@ -1,7 +1,15 @@
 from ultralytics import YOLO
 
-# Load a model
+import sys
+sys_args = sys.argv
+
+try:
+    no_of_epochs = int(sys_args[1])
+except:
+     no_of_epochs = 1
+
+# # Load a model
 model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
-# Use the model
+# # # Use the model
 results = model.train(data="config.yaml", epochs=1)  # train the model
